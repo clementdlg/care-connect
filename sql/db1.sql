47120b185d5b1789920a0b3d8ead3637ff71a2c6
Clément DE LA GENIERE <0xkrem@proton.me>
2025-02-13 22:20:15 +0100

    removed old sql files

--- sql/db1.sql
+++
@@ -1,6 +0,0 @@
--- Example SQL file, will be overwritten
-CREATE TABLE user(
-	id INTEGER PRIMARY KEY AUTO_INCREMENT,
-	name VARCHAR(255),
-	age INT
-);
--- sql/db2.sql
+++
@@ -1,3 +0,0 @@
--- Example SQL file, will be overwritten
-INSERT INTO user (name, age) VALUES("John", 25);
-INSERT INTO user (name, age) VALUES("Doe", 54);
